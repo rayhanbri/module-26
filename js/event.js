7ddd2f4d413ee1e894413816db12029cdd7a16df
rayhan <mohammadrayhanx0@gmail.com>
2025-10-15 01:10:47 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,14 @@
+// option 2 for handeling event 
+
+function makeyellow (){
+  document.body.style.backgroundColor = 'yellow';
+}
+
+function makered(){
+  document.body.style.backgroundColor = 'red';
+}
+
+
+
+   
+   
