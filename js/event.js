79f053ad0b5d624147ad870dddeffd6a87b2e2d2
rayhan <mohammadrayhanx0@gmail.com>
2025-10-17 18:22:35 +0600

    changes in index.html

--- js/event.js
+++ js/event.js
@@ -1,14 +1,44 @@
 // option 2 for handeling event 
 
-function makeyellow (){
-  document.body.style.backgroundColor = 'yellow';
+function makeGreen() {
+  document.body.style.backgroundColor = 'green';
 }
-
-function makered(){
+function makered() {
   document.body.style.backgroundColor = 'red';
 }
 
+// Make purple 
+const makePurple = document.getElementById('btn-purple');
+console.log(makePurple)
+
+makePurple.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple"
+}
 
 
-   
-   
+// makeblue 
+const btnMakeBlue = document.getElementById('btn-make-blue');
+console.log(btnMakeBlue)
+// call the function see the magic 
+btnMakeBlue.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = 'blue';
+}
+
+
+// add eventlistener 
+// id diye access korbo //.addEventListener ('event type',handler)
+document.getElementById('btn-make-green').addEventListener('click', function makeGreen() {
+  document.body.style.backgroundColor = 'green'
+})
+
+
+document.getElementById('btn-make-gold').addEventListener('click',function makeGold(){
+  document.body.style.backgroundColor = 'gold'
+})
+
+
+
+
+
